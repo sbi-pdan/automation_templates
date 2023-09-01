f7dc62b9d945cd72b894d6cdf14f5704e96ce503
Daniel Paes <danielpaes@solution-bi.com>
2023-09-01 10:23:37 -0400

    Moving wip files into dev branch

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python3 -m venv python_deps
-source python_deps/bin/activate
-pip install pre-commit
-pre-commit install
-pre-commit run
-# pre-commit run --all-files # To be added on CI pipelines.
